68f73db4478ac98a64da24e5fc781db613e92fcd
velcronator <valacos7@gmail.com>
2024-07-24 01:24:03 +1000

    Added ladder sprites and isrunning bool in player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float runSpeed = 350;
+    private Animator animator;
     
     Vector2 movementInput;
     // cache the player's rigidbody
@@ -15,23 +16,34 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         Run();
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
     }
 
     private void Run()
     {
         Vector2 playerVelocity = new Vector2(runSpeed * movementInput.x * Time.deltaTime, rb.velocity.y);
         rb.velocity = playerVelocity;
+        animator.SetBool("isRunning", movementInput.x != 0);
     }
 
     private void OnMove(InputValue value)
     {
         movementInput = value.Get<Vector2>();
-        Debug.Log("Player is moving with input: " + movementInput);
     }
 }
